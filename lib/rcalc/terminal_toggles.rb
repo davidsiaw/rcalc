2e8e2e877049be58fa0f22f6b3577d4d0239f956
David Siaw <874280+davidsiaw@users.noreply.github.com>
2026-05-02 11:37:24 +0900

    new toggles

--- lib/rcalc/terminal_toggles.rb
+++ lib/rcalc/terminal_toggles.rb
@@ -4,6 +4,8 @@ module Rcalc
   TERMINAL_TOGGLES = {
     mouse_button: '?1000;1006',
     mouse_event: '?1000;1003;1006',
-    focus_event: '?1004'
+    focus_event: '?1004',
+    alt_buffer: '?1049',
+    cursor: '?25'
   }.freeze
 end
